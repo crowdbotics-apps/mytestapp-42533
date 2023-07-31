8ec8e7db2ee313e7656c5623132a4880761d5070
Italo Batista <italo.batista@magicfloridaproperties.com>
2023-07-30 22:06:32 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "mytestapp-42533",
-  url: "https://mytestapp-42533.botics.co",
-  api: "https://mytestapp-42533.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "mytestapp-42533",
+  "url": "https://mytestapp-42533.botics.co",
+  "api": "https://mytestapp-42533.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Untitled3"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
